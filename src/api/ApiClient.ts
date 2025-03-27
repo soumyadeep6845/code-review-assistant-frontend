6a6d5063c824c5544049ec72ac6204fe6b895961
Soumyadeep Das <soumyadeep.dbsslg@gmail.com>
2025-03-27 13:09:29 +0530

    made user ID dynamic

--- src/api/ApiClient.ts
+++ src/api/ApiClient.ts
@@ -8,3 +8,14 @@ export const apiClient = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+// Interceptor to add Authorization header with token
+apiClient.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
--- src/utils/ProtectedRoute.tsx
+++ src/utils/ProtectedRoute.tsx
@@ -1,7 +1,18 @@
+import { jwtDecode } from "jwt-decode";
 import { Navigate, Outlet } from "react-router-dom";
 
+const isValidToken = (token: string) => {
+  try {
+    const decoded: any = jwtDecode(token);
+    return decoded.exp * 1000 > Date.now(); // Ensure token is not expired
+  } catch (error) {
+    return false;
+  }
+};
+
 const ProtectedRoute = () => {
-  const isAuthenticated = !!localStorage.getItem("token");
+  const token = localStorage.getItem("token");
+  const isAuthenticated = token && isValidToken(token);
 
   return isAuthenticated ? <Outlet /> : <Navigate to="/auth" replace />;
 };
